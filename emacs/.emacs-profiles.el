5aa4adc92a637f5ab9987e35250122932696886b
Pavel Novichkov <paul.novichkov@yandex.com>
2021-01-17 11:46:32 +0100

    Add emacs config

---
+++ emacs/.emacs-profiles.el
@@ -0,0 +1,4 @@
+(("default" . ((user-emacs-directory . "~/Documents/git/emacs.d")))
+ ("spacemacs" . ((user-emacs-directory . "~/Documents/git/spacemacs")))
+ ("doom" . ((user-emacs-directory . "~/Documents/git/doom-emacs")
+            (env . (("DOOMDIR" . "~/.doom.d"))))))
