5c4abbceae38665493e69d037b805a7895544b62
Pavel Novichkov <paul.novichkov@yandex.com>
2021-01-31 21:05:00 +0100

    Add user path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,6 @@
+# Add user path, like prepend $PATH in bash.
+contains ~/.local/bin/ $fish_user_paths; or set -Ua fish_user_paths ~/.local/bin/
+
 if test "$TERM" = "dumb"
   function fish_prompt
     echo "\$ "
