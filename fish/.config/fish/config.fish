388b29c68063fcb9ad3802a04f19816d9dcf026a
Pavel Novichkov <paul.novichkov@yandex.com>
2021-01-31 21:08:01 +0100

    Add comment

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 # Add user path, like prepend $PATH in bash.
 contains ~/.local/bin/ $fish_user_paths; or set -Ua fish_user_paths ~/.local/bin/
 
+# Fix emacs tramp: https://github.com/oh-my-fish/theme-bobthefish/issues/148.
 if test "$TERM" = "dumb"
     function fish_prompt
         echo "\$ "
