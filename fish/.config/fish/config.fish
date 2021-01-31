9963d4b0042cf4627b8af849301053b43f43cfa2
Pavel Novichkov <paul.novichkov@yandex.com>
2021-01-31 21:07:47 +0100

    Fix indentation

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,11 +2,11 @@
 contains ~/.local/bin/ $fish_user_paths; or set -Ua fish_user_paths ~/.local/bin/
 
 if test "$TERM" = "dumb"
-  function fish_prompt
-    echo "\$ "
-  end
+    function fish_prompt
+        echo "\$ "
+    end
 
-  function fish_right_prompt; end
-  function fish_greeting; end
-  function fish_title; end
+    function fish_right_prompt; end
+    function fish_greeting; end
+    function fish_title; end
 end
