bf817db5c5fc0a97b2b86cb01e3d18129641a362
Pavel Novichkov <paul.novichkov@yandex.com>
2021-10-25 15:30:09 +0200

    Make global word deletion bindings work in terminals

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,8 @@
 function fish_user_key_bindings
     fzf_key_bindings
+
+    # Add bindings for consistency with global text editing shortcuts,
+    # see https://en.wikipedia.org/wiki/Table_of_keyboard_shortcuts.
+    bind \b backward-kill-word # Control+Backspace
+    bind \e\[3\;5~ kill-word # Control+Delete
 end
